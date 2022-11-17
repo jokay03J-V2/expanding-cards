e8bec34e0c08a6a8ae18360112f5c2ccb114dcef
jokay03J-V2 <jmagro@edenschool.fr>
2022-11-17 17:13:43 +0100

    update js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 import "../css/style.css"
+const cards = document.getElementsByClassName("card")
+
+for (let index = 0; index < cards.length; index++) {
+    const card = cards[index];
+    card.addEventListener("click", (ev) => {
+        for (let iCard = 0; iCard < cards.length; iCard++) {
+            const c = cards[iCard]
+            console.log(c);
+            console.log(ev.currentTarget);
+
+            if (ev.currentTarget === c) {
+                c.classList.toggle("active")
+            } else 
+            if(c.classList.contains("active") && ev.currentTarget !== c){
+                c.classList.remove("active")
+            }
+        }
+    }, true)
+}
